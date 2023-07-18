baee0b16c1f2b9c9722c9a5c457627bee99707e2
DefectingCat <i@defect.ink>
2023-07-18 17:18:29 +0800

    add tcp frame

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use tokio::time::Instant;
+
 #[derive(Default)]
 pub enum PingMethod {
     #[default]
@@ -16,18 +18,33 @@ impl From<&str> for PingMethod {
     }
 }
 
+pub struct TcpFrame {
+    // request start time
+    pub start: Instant,
+    // elapsed time millis
+    pub elapsed: f32,
+}
+impl TcpFrame {
+    pub fn calculate_delay(&mut self) {
+        let nanos = self.start.elapsed().as_nanos();
+        let millis = nanos as f32 / 1000000 as f32;
+        self.elapsed = millis
+    }
+}
+
 pub struct Pluto {
     pub method: PingMethod,
     pub port: u32,
+    pub queue: Vec<TcpFrame>,
 }
 impl Default for Pluto {
     fn default() -> Self {
         Self {
             method: PingMethod::default(),
-            port: 80
+            port: 80,
+            queue: vec![],
         }
     }
-    
 }
 
 impl Pluto {}
