1fed085354985626e269f4f395ea21bbff51ebb1
DefectingCat <i@defect.ink>
2023-07-18 14:23:32 +0800

    add default struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
+#[derive(Default)]
+pub enum PingMethod {
+    #[default]
+    Tcp,
+    Http,
+    Unknow,
+}
+impl From<&str> for PingMethod {
+    fn from(value: &str) -> Self {
+        use PingMethod::*;
+        match value.to_lowercase().as_str() {
+            "tcp" => Tcp,
+            "http" => Http,
+            _ => Unknow,
+        }
+    }
+}
 
+#[derive(Default)]
+pub struct Pluto {
+    pub method: PingMethod,
+}
+
+impl Pluto {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use pluto::Pluto;
+use tokio::time::Instant;
+
 fn main() {
+    let start = Instant::now();
+    let pluto = Pluto::default();
     println!("Hello, world!");
+    let nanos = start.elapsed().as_nanos();
+    let millis = nanos as f32 / 1000000 as f32;
+    println!("{}", millis);
 }
