d80858604105e71c50c0ccd7469664fa28eb65f7
DefectingCat <i@defect.ink>
2023-07-18 11:37:34 +0800

    init tcp server

--- examples/server.rs
+++ examples/server.rs
@@ -1,7 +1,21 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
+
+    let listener = TcpListener::bind("127.0.0.1:4000").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            handler(stream).await?;
+            Ok::<()>(())
+        });
+    }
+}
+
+async fn handler(stream: TcpStream) -> Result<()> {
     Ok(())
 }
